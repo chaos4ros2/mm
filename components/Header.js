ab844e02172b27061193cba86c6da164d22112a9
chaos44 <liuchao46642@yahoo.co.jp>
2022-07-15 12:11:03 +0900

    add LocationHeader component basic

--- components/Header.js
+++ components/Header.js
@@ -15,24 +15,8 @@ const Header = ({}) => {
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <div className="w-auto grow flex items-center">
-                <div className="text-sm grow shrink-0 mr-4">
-                    <a href="#responsive-header" className="hidden mt-4 mr-4 text-teal-200 hover:text-white">
-                        Docs
-                    </a>
-                    <a href="#responsive-header" className="hidden mt-4 mr-4 text-teal-200 hover:text-white">
-                        Examples
-                    </a>
-                    <a href="#responsive-header" className="hidden mt-4 mr-4 text-teal-200 hover:text-white">
-                        Blog
-                    </a>
-                    <LocationHeader />
-                </div>
-                <div>
-                    <a href="#" className="flex-nowrap inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-0">Download</a>
-                </div>
-
-            </div>
+            { /* Todo：分岐追加 */ }
+            <LocationHeader />
         </nav>
     ); 
 };
--- components/header/LocationHeader.js
+++ components/header/LocationHeader.js
@@ -1,8 +1,23 @@
+import {useEffect, useState} from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+
 const LocationHeader = ({  }) => {
     // const {title, description, icon } = meta;
+    const [ showItemFlag, setShowItemFlag ] = useState(false);
     
+    const toggle_member_search_form = () => setShowItemFlag(!showItemFlag)
+
     return (
-        <div className="ml-32 font-semibold text-lg text-white">メンバー名簿</div>
+        <div className="w-auto grow flex items-center">
+            <div className="text-sm grow shrink-0 mr-4">
+                <div className={`ml-32 font-semibold text-lg text-white ${showItemFlag ? 'hidden' : ''}`}>メンバー名簿</div>
+                {/* Todo：input欄に文字が入力されるとメンバー検索のイベントをアクティブにする */}
+                <input className={`ml-24 font-semibold text-lg ${!showItemFlag ? 'hidden' : ''}`} /> 
+            </div>
+            <div>
+                <SearchIcon onClick={toggle_member_search_form} className="text-white mr-4 cursor-pointer"/>
+            </div>
+        </div>
     ); 
 };
 
