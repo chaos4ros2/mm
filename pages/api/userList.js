6aeb7f47458347d76e9d8ebaad7f525194bf5734
chaos4ros2 <liuchao4664@yahoo.co.jp>
2022-08-13 08:50:05 +0900

    prisma test

--- pages/api/userList.js
+++ pages/api/userList.js
@@ -1,7 +1,17 @@
-// import prisma from '../../lib/prisma';
+import prisma from '../../lib/prisma';
 
 // POST /api/userList
 // Required fields in body: id, name
 export default async function handle(req, res) {
-    res.status(200).json({message: 'Profile updated successfully'});
+    const user_list = await prisma.Employee.findMany({
+        where: {
+            delete_date: null,
+        },
+        select: {
+            id: true,
+            name: true,
+            phone_number: true,
+        }
+    });
+    res.status(200).json(user_list);
 }
