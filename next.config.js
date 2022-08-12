e514796cf6a0ad96f26e6380f84c9246f094c5f7
chaos4ros2 <liuchao4664@yahoo.co.jp>
2022-08-12 12:47:52 +0900

    fix cross origin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/api/(.*)",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "https://mm-taupe.vercel.app/" },
+                ]
+            }
+        ]
+    },
 }
 
 module.exports = nextConfig
