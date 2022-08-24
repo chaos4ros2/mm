47dda43e2c8898debf888f23465b89bd2b031d99
chaos4ros2 <liuchao4664@yahoo.co.jp>
2022-08-25 00:04:47 +0900

    add imformation page's icon

--- components/header/FacilityHeader.js
+++ components/header/FacilityHeader.js
@@ -1,6 +1,10 @@
 import {useEffect, useState} from 'react';
+import { useRouter } from 'next/router'
+import CalendarMonth from '@mui/icons-material/CalendarMonth';
 
 const FacilityHeader = ( { name } ) => {
+    const router = useRouter();
+    const { id, name } = router.query;
     // const {title, description, icon } = meta;
 
     return (
@@ -8,6 +12,11 @@ const FacilityHeader = ( { name } ) => {
             <div className="text-sm grow shrink-0 mr-4">
                 <div className={`ml-32 font-semibold text-lg text-white`}>{name}</div>
             </div>
+            <a href={`/information/${id}`} target='_blank' rel='noreferrer'>
+                <CalendarMonth 
+                    className="text-white mr-4 cursor-pointer"
+                />
+            </a>
         </div>
     ); 
 };
